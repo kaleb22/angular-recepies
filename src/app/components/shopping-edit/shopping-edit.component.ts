87c6c22c1e6e8fbfbe53229febcfab5bfaabecb3
kaleb22 <kaleb22@hotmail.com>
2023-01-27 12:32:35 -0300

    feat: btn adding ingredients functionality

--- src/app/components/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/model/ingredient.model';
+import { EventEmitter } from '@angular/core';
+import { Component, ElementRef, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,7 +11,17 @@ export class ShoppingEditComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   ngOnInit(): void {
   }
 
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const ingredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(ingredient);
+  }
 }
