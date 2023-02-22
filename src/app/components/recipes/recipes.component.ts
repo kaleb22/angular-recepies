d0f43a7fdfc7c97b85f858d88686353b71518954
kaleb22 <kaleb22@hotmail.com>
2023-02-22 14:06:34 -0300

    refac: removing unused code

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -1,30 +1,9 @@
-import { RecipeService } from './../../shared/recipe.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Recipe } from 'src/app/model/recipe.model';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent implements OnInit, OnDestroy {
-
-  constructor(private recipeService: RecipeService) { }
-
-  selectedRecipe: Recipe;
-  sub: Subscription;
-
-  ngOnInit(): void {
-    this.sub = this.recipeService.recipeSelected.subscribe(
-      recipe => {
-        this.selectedRecipe = recipe;
-      }
-    )
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
-
+export class RecipesComponent {
 }
--- src/app/shared/recipe.service.ts
+++ src/app/shared/recipe.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../model/ingredient.model';
 import { Recipe } from '../model/recipe.model';
 import { ShoppingListService } from './shopping-list.service';
@@ -9,8 +9,6 @@ import { ShoppingListService } from './shopping-list.service';
 export class RecipeService {
 
   constructor(private slService: ShoppingListService) { }
-
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
